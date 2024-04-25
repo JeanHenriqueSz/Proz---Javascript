be98a29c4349efca883cefea9cc57b1f5620872e
JeanHenriqueSz <jeanhenrique98@gmail.com>
2024-04-25 19:41:43 -0300

    commit

---
+++ code-js1/script.js
@@ -0,0 +1,9 @@
+// Definindo a função para imprimir a mensagem
+function imprimirMensagem() {
+    console.log("Conexão feita com sucesso!");
+}
+
+// Executando a função três vezes
+imprimirMensagem();
+imprimirMensagem();
+imprimirMensagem();
